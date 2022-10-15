5cf810c2718602fb90b098bdfd805a34658537f3
Wellwishes153 <vithuc1503@gmail.com>
2022-10-15 15:07:09 +0900

    first commit

---
+++ ps0.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 15 14:59:10 2022
+
+@author: Demo
+"""
+import numpy
+x = int(input("Nhap x : "))
+y = int(input("Nhap y : "))
+print((x**y))
+print(round(numpy.log(x)))
